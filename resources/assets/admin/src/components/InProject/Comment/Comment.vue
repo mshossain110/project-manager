2c50bf9b3dd2b2fecd75071fef8580a45b933adf
mshossain110 <mshossain110@gmail.com>
2019-03-23 15:16:10 +0600

    Showing comments in discussion

---
+++ resources/assets/admin/src/components/InProject/Comment/Comment.vue
@@ -0,0 +1,38 @@
+<template>
+    <VCard flat>
+        <VCardText primary-title>
+            <div class="descriptipn">
+                <div> {{ comment.content }} </div>
+            </div>
+            <div class="commenttime">
+                <span>{{ momentFormat(comment.created_at.date, 'DD MMM YYYY') }}</span>
+                <span>{{ momentFormat(comment.created_at.date, 'HH: MM A') }}</span>
+            </div>
+        </VCardText>
+    </VCard>
+</template>
+
+<script>
+export default {
+    props: {
+        comment: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    created () {
+
+    },
+    methods: {
+
+    }
+}
+</script>
---
+++ resources/assets/admin/src/components/InProject/Comment/Comments.vue
@@ -0,0 +1,96 @@
+<template>
+    <VLayout class="comment-container">
+        <VFlex xs12>
+            <VContainer
+                id="scroll-target"
+                style="max-height: 400px"
+                class="scroll-y"
+            >
+                <VTimeline
+                    dense
+                >
+                    <VTimelineItem
+                        v-for="comment in comments"
+                        :key="comment.id"
+                        class="comment"
+                        fill-dot
+                    >
+                        <template v-slot:icon>
+                            <VAvatar>
+                                <img src="http://i.pravatar.cc/64">
+                            </VAvatar>
+                        </template>
+
+                        <Comment :comment="comment" />
+                    </vtimelineitem>
+                </VTimeline>
+            </VContainer>
+            <CommentForm
+                commentable-type="discussion_board"
+                :commentable-id="commentableId"
+            />
+        </VFlex>
+    </VLayout>
+</template>
+
+<script>
+import Comment from './Comment'
+import CommentForm from './CommentForm'
+export default {
+    components: {
+        Comment,
+        CommentForm
+    },
+    props: {
+        comments: {
+            type: Array,
+            required: true
+        },
+        commentableType: {
+            type: String,
+            default () {
+                return 'discussion_board'
+            }
+        },
+        commentableId: {
+            type: Number,
+            required: true
+        }
+    },
+    data () {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    created () {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="stylus">
+.comment-container
+    .v-timeline
+        padding-top 0
+        min-height: 64px
+        &:before
+            display none
+        .v-timeline-item
+            padding-bottom 5px
+            .v-card
+                &:before
+                &:after
+                    display none
+                .v-card__text
+                    padding: 10px 2px
+                    .commenttime
+                        text-align right
+                        font-size 10px
+            &:hover .v-card__text
+                background-color #f3f3f3
+</style>
